bef015ed018d142a85045aa420a250d93b4358ae
tedd mason <teddmason@gmail.com>
2021-08-17 10:28:19 +0100

    additional changes for concurrent execution

--- database/flooddev/u_flood/4.0.0/new_indexes.sql
+++ database/flooddev/u_flood/4.0.0/new_indexes.sql
@@ -15,3 +15,24 @@ CREATE UNIQUE INDEX IF NOT EXISTS idx_rainfall_stations_unique
     ON u_flood.rainfall_stations_mview USING btree
     (telemetry_station_id ASC NULLS LAST)
     TABLESPACE flood_indexes;
+
+
+-- Index: idx_telemetry_context_unique
+
+-- DROP INDEX u_flood.idx_telemetry_context_unique;
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_telemetry_context_unique
+    ON u_flood.telemetry_context_mview USING btree
+    (rloi_id ASC NULLS LAST)
+    TABLESPACE flood_indexes;
+
+-- Index: idx_station_split_unique
+
+-- DROP INDEX u_flood.idx_station_split_unique;
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_station_split_unique
+    ON u_flood.station_split_mview USING btree
+    (rloi_id ASC NULLS LAST, qualifier COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE flood_indexes;
+
+
