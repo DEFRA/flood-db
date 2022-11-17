482d69e4539b9fbad80038b4ae43e74c9940257a
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2022-11-17 16:25:00 +0000

    Fixes to issues encountered when running scripts
    * Column name change in data load script to reflect current state of table
    * Fixed invalid SQL and added index for new river_id column

--- database/flooddev/u_flood/4.3.0/river_stations.sql
+++ database/flooddev/u_flood/4.3.0/river_stations.sql
@@ -1,1 +1,8 @@
-ALTER TABLE u_flood.river_stations ADD COLUMN IF NOT EXISTS ADD river_id integer, ADD local_name character varying(200);
+ALTER TABLE u_flood.river_stations
+  ADD COLUMN IF NOT EXISTS river_id integer,
+  ADD COLUMN IF NOT EXISTS qualified_name character varying(200);
+
+CREATE INDEX IF NOT EXISTS idx_river_id
+    ON u_flood.river_stations USING btree
+    (river_id ASC NULLS LAST)
+    TABLESPACE flood_indexes;
