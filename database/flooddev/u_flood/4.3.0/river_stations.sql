3588f27f19a48af28798d4c4218041e62e7a6b25
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2022-11-18 00:11:14 +0000

    Proposed mview changes
    Notes:
    
    * i'm dropping and recreating the river_stations table as it was easiest
      to do but happy to revisit that
    * the river_stations table does not need the name columns now that we
      have the river table and can do a join
    * this means that the psql import uses cut to import a subset of the
      columns. we could just edit the csv instead
    * creating and dropping mviews (and tables) needs to be sequenced
    * i've run all these on a local copy of the tst db but i'd like to go
      through the steps again on monday to make sure i haven't forgotten
      anything.
    * flood-service continues to work after this but flood-app will need to
      be modified to make use of the qualified name

--- database/flooddev/u_flood/4.3.0/river_stations.sql
+++ database/flooddev/u_flood/4.3.0/river_stations.sql
@@ -1,8 +1,42 @@
-ALTER TABLE u_flood.river_stations
-  ADD COLUMN IF NOT EXISTS river_id integer,
-  ADD COLUMN IF NOT EXISTS qualified_name character varying(200);
+-- Table: u_flood.river_stations
+
+-- DROP MATERIALIZED VIEW IF EXISTS u_flood.stations_list_mview;
+-- DROP MATERIALIZED VIEW IF EXISTS u_flood.rivers_mview;
+DROP TABLE IF EXISTS u_flood.river_stations;
+
+CREATE TABLE IF NOT EXISTS u_flood.river_stations
+(
+    id character varying(200) COLLATE pg_catalog."default",
+    rloi_id integer,
+    rank integer,
+    river_id integer
+)
+
+TABLESPACE flood_tables;
+
+ALTER TABLE IF EXISTS u_flood.river_stations
+    OWNER to u_flood;
+-- Index: idx_river_id
+
+-- DROP INDEX IF EXISTS u_flood.idx_river_id;
 
 CREATE INDEX IF NOT EXISTS idx_river_id
     ON u_flood.river_stations USING btree
     (river_id ASC NULLS LAST)
+    TABLESPACE flood_indexes;
+-- Index: idx_river_stations_id
+
+-- DROP INDEX IF EXISTS u_flood.idx_river_stations_id;
+
+CREATE INDEX IF NOT EXISTS idx_river_stations_id
+    ON u_flood.river_stations USING btree
+    (id COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE flood_indexes;
+-- Index: idx_river_stations_rloi_id
+
+-- DROP INDEX IF EXISTS u_flood.idx_river_stations_rloi_id;
+
+CREATE INDEX IF NOT EXISTS idx_river_stations_rloi_id
+    ON u_flood.river_stations USING btree
+    (rloi_id ASC NULLS LAST)
     TABLESPACE flood_indexes;
