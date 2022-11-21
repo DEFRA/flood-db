a4682ae657cc5becb53b4f42ec18ec5c29fc49ad
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2022-11-21 23:31:08 +0000

    Remove integer river id from river_stations
    * Work to use integer river_id is deferred, will carry on using
      character id
    * Ensure we refresh both mviews (rivers_mview and stations_list_mview)
      which are dependant on the two data tables
    * Add a drop script to ensure everything is dropped in the right order
    * Make river.id a primary key to allow postgres to create the index
    
    Note: river_station.id should become river_station.river_id in a future
    commit

--- database/flooddev/u_flood/4.3.0/river_stations.sql
+++ database/flooddev/u_flood/4.3.0/river_stations.sql
@@ -1,29 +1,22 @@
 -- Table: u_flood.river_stations
 
--- DROP MATERIALIZED VIEW IF EXISTS u_flood.stations_list_mview;
--- DROP MATERIALIZED VIEW IF EXISTS u_flood.rivers_mview;
-DROP TABLE IF EXISTS u_flood.river_stations;
-
 CREATE TABLE IF NOT EXISTS u_flood.river_stations
 (
+    -- note: this should be river_id
     id character varying(200) COLLATE pg_catalog."default",
     rloi_id integer,
-    rank integer,
-    river_id integer
+    rank integer
 )
 
 TABLESPACE flood_tables;
 
 ALTER TABLE IF EXISTS u_flood.river_stations
     OWNER to u_flood;
+
 -- Index: idx_river_id
 
 -- DROP INDEX IF EXISTS u_flood.idx_river_id;
 
-CREATE INDEX IF NOT EXISTS idx_river_id
-    ON u_flood.river_stations USING btree
-    (river_id ASC NULLS LAST)
-    TABLESPACE flood_indexes;
 -- Index: idx_river_stations_id
 
 -- DROP INDEX IF EXISTS u_flood.idx_river_stations_id;
--- database/flooddev/u_flood/4.3.0/rivers.sql
+++ database/flooddev/u_flood/4.3.0/rivers.sql
@@ -1,10 +1,8 @@
 -- Table: u_flood.river
 
-DROP TABLE IF EXISTS u_flood.river CASCADE;
-
 CREATE TABLE IF NOT EXISTS u_flood.river
 (
-    id CHARACTER VARYING(200) COLLATE pg_catalog."default",
+    id CHARACTER VARYING(200) COLLATE pg_catalog."default" primary key,
     name CHARACTER VARYING(200) COLLATE pg_catalog."default",
     qualified_name CHARACTER VARYING(250) COLLATE pg_catalog."default"
 )
@@ -12,10 +10,3 @@ TABLESPACE flood_tables;
 
 ALTER TABLE IF EXISTS u_flood.river
     OWNER to u_flood;
-
-DROP INDEX IF EXISTS idx_river_id;
-
-CREATE INDEX IF NOT EXISTS idx_river_id
-    ON u_flood.river USING btree
-    (id COLLATE pg_catalog."default" ASC NULLS LAST)
-    TABLESPACE flood_indexes;
