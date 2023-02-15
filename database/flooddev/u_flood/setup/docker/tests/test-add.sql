c911fe0fd06d48e6be829669cce5f86dfe18f528
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2023-02-15 17:22:30 +0000

    Bubble trend up to stations_list_mview
    Add handling for trend value in rainfall gauges

--- database/flooddev/u_flood/setup/docker/tests/test-add.sql
+++ database/flooddev/u_flood/setup/docker/tests/test-add.sql
@@ -1,5 +1,5 @@
 BEGIN;
-  SELECT plan(9);
+  SELECT plan(12);
   SELECT has_materialized_view('stations_overview_mview');
   SELECT has_column( 'stations_overview_mview', 'value' );
   SELECT has_column( 'stations_overview_mview', 'value_timestamp' );
@@ -7,6 +7,7 @@ BEGIN;
   SELECT has_column( 'stations_overview_mview', 'previous_value' );
   SELECT has_column( 'stations_overview_mview', 'trend' );
   SELECT has_column( 'rivers_mview', 'trend' );
+  SELECT has_column( 'stations_list_mview', 'trend' );
   SELECT results_eq('SELECT count(*) FROM stations_overview_mview', ARRAY[ 2721::BIGINT ], 'stations_list_mview result count');
   SELECT results_eq(
     'SELECT rloi_id, processed_value, value_timestamp, previous_value, trend FROM stations_overview_mview WHERE rloi_id IN (1001,1006,1009) ORDER BY rloi_id',
@@ -24,5 +25,19 @@ BEGIN;
      (1009, 0.066, TO_TIMESTAMP('2023-02-06 13:15:00+00','YYYY-MM-DD HH24:MI:SS'), 'steady')
     $$,
     'rivers_mview results');
+  SELECT results_eq(
+    'SELECT rloi_id, value, value_timestamp, trend FROM stations_list_mview WHERE rloi_id IN (1001,1006,1009) ORDER BY rloi_id',
+    $$VALUES
+     (1001, 1.458, TO_TIMESTAMP('2023-02-06 13:15:00+00','YYYY-MM-DD HH24:MI:SS'), 'falling'),
+     (1006, 0.441, TO_TIMESTAMP('2023-02-06 13:15:00+00','YYYY-MM-DD HH24:MI:SS'), 'rising'),
+     (1009, 0.066, TO_TIMESTAMP('2023-02-06 13:15:00+00','YYYY-MM-DD HH24:MI:SS'), 'steady')
+    $$,
+    'stations_list_mview results');
+  SELECT results_eq(
+    $$SELECT river_id, value, value_timestamp, trend FROM stations_list_mview WHERE river_id = 'rainfall-North East' and telemetry_id = '075233'$$,
+    $$VALUES
+     ('rainfall-North East', 0.0, TO_TIMESTAMP('2023-02-06 13:15:00+00','YYYY-MM-DD HH24:MI:SS'), 'n/a')
+    $$,
+    'stations_list_mview rainfall results should set trend to n/a');
 SELECT * FROM finish();
 ROLLBACK;
