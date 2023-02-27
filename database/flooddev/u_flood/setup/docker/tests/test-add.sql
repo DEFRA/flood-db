cedbf64e754f7a9edf9345c834737a0ce2cee7b9
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2023-02-27 19:15:04 +0000

    Add test for suspended station

--- database/flooddev/u_flood/setup/docker/tests/test-add.sql
+++ database/flooddev/u_flood/setup/docker/tests/test-add.sql
@@ -1,5 +1,5 @@
 BEGIN;
-  SELECT plan(12);
+  SELECT plan(13);
   SELECT has_materialized_view('stations_overview_mview');
   SELECT has_column( 'stations_overview_mview', 'value' );
   SELECT has_column( 'stations_overview_mview', 'value_timestamp' );
@@ -42,10 +42,16 @@ BEGIN;
     $$,
     'stations_list_mview results');
   SELECT results_eq(
+    'SELECT rloi_id, value, status, value_timestamp, trend FROM stations_list_mview WHERE rloi_id IN (9302) ORDER BY rloi_id',
+    $$VALUES
+     (9302, 78.8, 'Suspended', TO_TIMESTAMP('2023-02-06 10:00:00+00','YYYY-MM-DD HH24:MI:SS'), 'rising')
+    $$,
+    'stations_list_mview should populate trend for suspended groundwater station');
+  SELECT results_eq(
     $$SELECT river_id, value, value_timestamp, trend FROM stations_list_mview WHERE river_id = 'rainfall-North East' and telemetry_id = '075233'$$,
     $$VALUES
      ('rainfall-North East', 0.0, TO_TIMESTAMP('2023-02-06 13:15:00+00','YYYY-MM-DD HH24:MI:SS'), 'n/a')
     $$,
-    'stations_list_mview rainfall results should set trend to n/a');
+    'stations_list_mview should set trend to n/a for rainfall stations');
 SELECT * FROM finish();
 ROLLBACK;
